d2c442b5299a1d2de7cb05148d4ef54225a6328a
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2022-07-12 16:40:50 +0200

    Building Reg Form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,16 +7,36 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-registerForm = new FormGroup({
-name: new FormControl('', [
-  Validators.required,
-  Validators.minLength(3),
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('', [
+    Validators.min(18),
+    Validators.max(120)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirm_password = new FormControl('', [
+    Validators.required
+  ])
+  
+  phoneNumber = new FormControl('')
 
-]),
-email: new FormControl(''),
-age: new FormControl(''),
-password: new FormControl(''),
-confirm_password: new FormControl(''),
-phoneNumber: new FormControl('')
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+
 })
+
 }
